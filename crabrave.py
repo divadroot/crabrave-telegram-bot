f9679627fb39d409ee5b04b54cc9ebbe7aee0c8b
David Sn <divad.nnamtdeis@gmail.com>
2019-10-12 04:21:38 +0200

    Add Bag Raiders style (Shooting stars)
    Signed-off-by: David Sn <divad.nnamtdeis@gmail.com>

--- crabrave.py
+++ crabrave.py
@@ -177,6 +177,14 @@ def inlinequery(update, context):
             video_url=f"{BASE_URL}/video/{parse.quote_plus(query)}.mp4?style=onabytakchciala&font=comicsans&color=white&size=52&filter=snapchat",
             mime_type="video/mp4"
         ),
+        InlineQueryResultVideo(
+            id=uuid4(),
+            title="🦀🦀 BAG RAIDERS IS GONE 🦀🦀",
+            description=query,
+            thumb_url=f"{BASE_URL}/thumb/shootingstars.jpg?t={time.time()}",
+            video_url=f"{BASE_URL}/video/{parse.quote_plus(query)}.mp4?style=shootingstars&font=spacemono&color=white&size=52&filter=simple",
+            mime_type="video/mp4"
+        ),
         InlineQueryResultArticle(
             id=uuid4(),
             title="Bot made by @divadsn",
